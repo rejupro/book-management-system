fc6338b2c3f10ae92c7a996b7ceacbeab6f359e1
Md. Reedwanul Haque <reedwanultpi@gmail.com>
2025-05-27 07:42:48 +0600

    Add Confirmation Message on plugin deactivation

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+  // Function to handle the click event on the button
+  $("#deactivate-book-management-system").on("click", function (e) {
+    e.preventDefault(); // Prevent the default action of the button
+
+    var hasConfirmed = confirm(
+      "Are you sure you want to deactivate the Book Management System? This action cannot be undone."
+    );
+    if (hasConfirmed) {
+      // If the user confirms, redirect to the deactivation URL
+      window.location.href = $(this).attr("href");
+    }
+  });
+});
